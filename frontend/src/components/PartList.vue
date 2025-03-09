f0bd9673e16620f0eca92b2cc1700f810257fbf4
adi3947 <adi3947@gmail.com>
2025-03-09 12:36:01 +0700

    Initial commit

---
+++ frontend/src/components/PartList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Part No</th>
+                    <th>jO</th>
+                    <th></th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+    
+</style>
